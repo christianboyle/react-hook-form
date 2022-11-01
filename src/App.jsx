210d109e531d7dbd7d3f793ccba2a41c46224512
Christian Boyle <mail@christianboyle.com>
2022-11-01 16:37:44 -0500

    add form validations

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,35 @@ export default function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className='form-control'>
           <label>Email</label>
-          <input type='text' name='email' {...register('email')} />
+          <input
+            type='text'
+            name='email'
+            {...register('email', {
+              required: 'Email is required.',
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: 'Email is not valid.'
+              }
+            })}
+          />
+          {errors.email && <p className='errorMsg'>{errors.email.message}</p>}
         </div>
         <div className='form-control'>
           <label>Password</label>
-          <input type='password' name='password' {...register('password')} />
+          <input
+            type='password'
+            name='password'
+            {...register('password', {
+              required: 'Password is required.',
+              minLength: {
+                value: 6,
+                message: 'Password should be at-least 6 characters.'
+              }
+            })}
+          />
+          {errors.password && (
+            <p className='errorMsg'>{errors.password.message}</p>
+          )}
         </div>
         <div className='form-control'>
           <label></label>
