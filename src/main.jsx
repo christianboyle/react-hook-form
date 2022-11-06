c4cb4d25da6b3d824269138923772b81b749d31d
Christian Boyle <mail@christianboyle.com>
2022-11-06 09:27:42 -0600

    add select input

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import App from './App'
+import Select from './Select'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Select />
   </React.StrictMode>
 )
